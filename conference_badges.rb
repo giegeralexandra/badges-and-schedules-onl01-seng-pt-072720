f7db1ef523b9e84c9130d672871d9d86c362ca68
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-03 22:46:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.each{|name| return "Hello, my name is #{name}."}
+  new_array = []
+  array.each{|name| puts "Hello, my name is #{name}."}
+  return
 end
